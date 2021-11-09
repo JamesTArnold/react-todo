412d2c510572180441097f0574e0540216df0cdd
James T. Arnold <jamestarnold85@gmail.com>
2021-11-09 14:59:45 -0600

    add tasks and firebase

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -3,8 +3,10 @@ import { useState } from "react";
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,10 +27,42 @@ const App = () => {
       reminder: false,
     },
   ]);
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+    console.log(id);
+  };
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
   return (
     <div className="container">
-      <Header title={"React Todo"} />
-      <Tasks tasks={tasks} />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+        title={"React Todo"}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder}
+          onAdd={addTask}
+        />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 };
