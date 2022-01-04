b74fa60f050e41d645801d9a41c401f7e29b2fa0
James T. Arnold <jamestarnold85@gmail.com>
2022-01-04 17:06:35 -0600

    added react Logo

--- react-todo/src/components/Header.js
+++ react-todo/src/components/Header.js
@@ -4,7 +4,10 @@ import Button from "./Button";
 export const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
-      <h1>{title}</h1>
+      <div className="header-text">
+        <img src="../logo192.png" alt="React Logo" width="50" height="50" />
+        <h1>{title}</h1>
+      </div>
       <Button
         color={showAdd ? "red" : "green"}
         text={showAdd ? "Close" : "Add"}
